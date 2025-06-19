867d4ea1ecd9b9baa082e646dfc31b8b3afd3431
Faiyaz Mahmud <faiyazmahmud2002@gmail.com>
2025-06-20 01:12:22 +0600

    books dto created

--- apps/books/src/books/dto/create-book.dto.ts
+++ apps/books/src/books/dto/create-book.dto.ts
@@ -1,1 +1,18 @@
-export class CreateBookDto {}
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from "class-validator";
+
+export class CreateBookDto {
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(300)
+    bookName:string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(200)
+    authorName:string;
+
+    @IsInt()
+    @Min(0)
+    quantity:number;
+}
--- apps/books/src/books/dto/update-book.dto.ts
+++ apps/books/src/books/dto/update-book.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookDto } from './create-book.dto';
+import { IsUUID } from 'class-validator';
 
 export class UpdateBookDto extends PartialType(CreateBookDto) {
-  id: number;
+    
+    @IsUUID()
+    id: string;
 }
